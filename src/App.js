1b2835e412e31e8385832d130515e2d65e430dcf
applabappweb <applabappweb@gmail.com>
2021-10-12 20:04:38 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcom Belefti to your dashboard.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Belefti 2.0
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
