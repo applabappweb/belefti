4c4c26a5a18003830eb0bfc716a05fb5b72b7d95
applabappweb <applabappweb@gmail.com>
2021-10-13 23:19:18 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -9,10 +9,19 @@ function App() {
   }
 
   const [earnings, setEarnings] = useState([])
-    useEffect(()=>{axios.get("https://hiveon.net/api/v1/stats/miner/1ad68e074d71c8fc6abe15187173767101d4c26e/ETH/billing-acc").then(res=>setEarnings(res.data)).catch(err=>console.log(err))}, [])
-    const tot=earnings.totalUnpaid;
-    const act = new Date(); 
-    const now =`${act.getDate()}/${act.getMonth()+1}/${act.getFullYear()} à ${((act.getHours() < 10)?"0":"") + act.getHours()}:${((act.getMinutes() < 10)?"0":"") + act.getMinutes()}:${((act.getSeconds() < 10)?"0":"") + act.getSeconds()}`;
+  const [prices, setPrices] = useState([])
+
+  useEffect(()=>{
+    axios.get("https://hiveon.net/api/v1/stats/miner/1ad68e074d71c8fc6abe15187173767101d4c26e/ETH/billing-acc").then(res=>setEarnings(res.data)).catch(err=>console.log(err));
+  
+    axios.get("https://api.ethereumdb.com/v1/ticker?pair=ETH-USD&range=1h").then(res=>setPrices(res.data)).catch(err=>console.log(err))
+  }, [])
+
+  const cur = prices.price;
+  const tot=earnings.totalUnpaid;
+  const act = new Date(); 
+  const now =`${act.getDate()}/${act.getMonth()+1}/${act.getFullYear()} à ${((act.getHours() < 10)?"0":"") + act.getHours()}:${((act.getMinutes() < 10)?"0":"") + act.getMinutes()}:${((act.getSeconds() < 10)?"0":"") + act.getSeconds()}`;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,9 +31,12 @@ function App() {
         <h4>
           {`La valeur actuelle est de: `}<span style={{color: "green"}}>{parseFloat(tot).toFixed(5)}</span>
         </h4>
-        <h5>
+        <h4>
           Le pourcentage pour atteindre 0.1 est de: <span style={{color: "red"}}>{(parseFloat(tot*1000).toFixed(2))} %</span>
-        </h5>
+        </h4>
+        <h4>
+          Le Montant est de: <span style={{color: "yellow"}}>{(parseFloat(cur*tot*180).toFixed(2))} DA</span>
+        </h4>
       </header>
       
     </div>
